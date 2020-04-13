cabb05a2d126ff3163baa49c03da298453c8efba
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2020-04-13 22:34:05 +0200

    Initial files for a worhskop

---
+++ webpack-react/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>Hello I'm React application</div>
+        )
+    }
+}
+export default App
