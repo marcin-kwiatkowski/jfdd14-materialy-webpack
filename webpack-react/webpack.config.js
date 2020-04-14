8a133634c44cb78bd9cf1263274c53b26b948946
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2020-04-14 10:41:15 +0200

    Added preset-react

--- webpack-react/webpack.config.js
+++ webpack-react/webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'script.js'
-    },
-    module: {
-      rules: [{
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          query: {
-              cacheDirectory: true,
-              presets: ["@babel/preset-env"]
-          }
-      }]
-  }
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "script.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+};
